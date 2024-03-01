fa31f53cc17616bf619458717e2dbb18ba674092
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2024-03-01 17:29:26 +0200

    style: calendar cells

--- lib/features/calendar/view/calendar_screen.dart
+++ lib/features/calendar/view/calendar_screen.dart
@@ -4,7 +4,6 @@ import 'package:keym_calendar/features/calendar/bloc/calendar_bloc.dart';
 import 'package:keym_calendar/features/calendar/widgets/add_event_button.dart';
 import 'package:keym_calendar/features/day_events/view/day_events_screen.dart';
 import 'package:keym_calendar/repositories/calendar/abstarct_calendar_repository.dart';
-import 'package:keym_calendar/repositories/calendar/models/event.dart';
 
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -32,62 +31,73 @@ class _CalendarScreenState extends State<CalendarScreen> {
     DateTime focusedDay = DateTime.now();
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: const Text('Calendar'),
         actions: const [
           AddEventButton(),
         ],
       ),
-      body: BlocBuilder<CalendarBloc, CalendarState>(
-        bloc: _calendarBloc,
-        builder: (context, state) {
-          if (state is CalendarLoaded) {
-            return TableCalendar(
-              onDaySelected: (selectedDay, focusedDay) {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => DayEventsScreen(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: BlocBuilder<CalendarBloc, CalendarState>(
+            bloc: _calendarBloc,
+            builder: (context, state) {
+              if (state is CalendarLoaded) {
+                return TableCalendar(
+                  headerStyle: const HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                  ),
+                  rowHeight: 55,
+                  daysOfWeekHeight: 35,
+                  onDaySelected: (selectedDay, focusedDay) {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => DayEventsScreen(
                           selectedDate: selectedDay,
-                        )));
-              },
-              firstDay: DateTime.utc(2020, 1, 1),
-              lastDay: DateTime.utc(2030, 12, 31),
-              focusedDay: focusedDay,
-              calendarBuilders: CalendarBuilders(
-                markerBuilder: (context, date, events) {
-                  final eventsCount = state.events
-                      .where((event) =>
-                          event.dateTime.year == date.year &&
-                          event.dateTime.month == date.month &&
-                          event.dateTime.day == date.day)
-                      .length;
-
-                  return Positioned(
-                    bottom: 1,
-                    child: Text(
-                      '$eventsCount',
-                      style: const TextStyle(fontSize: 10),
-                    ),
-                  );
-                },
-              ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+                        ),
+                      ),
+                    );
+                  },
+                  firstDay: DateTime.utc(2020, 1, 1),
+                  lastDay: DateTime.utc(2030, 12, 31),
+                  focusedDay: focusedDay,
+                  calendarBuilders: CalendarBuilders(
+                    markerBuilder: (context, date, events) {
+                      final eventsCount = state.events
+                          .where((event) =>
+                              event.dateTime.year == date.year &&
+                              event.dateTime.month == date.month &&
+                              event.dateTime.day == date.day)
+                          .length;
+                      return Positioned(
+                        bottom: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: List.generate(
+                              eventsCount > 3 ? 3 : eventsCount, (index) {
+                            return Padding(
+                              padding:
+                                  EdgeInsets.only(right: index < 2 ? 2 : 0),
+                              child: const Icon(
+                                Icons.circle,
+                                size: 10,
+                                color: Color.fromARGB(255, 110, 73, 139),
+                              ),
+                            );
+                          }),
+                        ),
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ),
       ),
-    );
-  }
-
-  Widget buildEventList(List<Event> events) {
-    return ListView.builder(
-      itemCount: events.length,
-      itemBuilder: (context, index) {
-        Event event = events[index];
-        return ListTile(
-          title: Text(event.title),
-          subtitle: Text(event.description),
-        );
-      },
     );
   }
 }
