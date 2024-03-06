d777d793bbfa197b88f875c6979d4dce39df1a6e
Zhenya-Mykhalskyi <zmykhalskyi@gmail.com>
2024-03-06 11:51:25 +0200

    feat: add event form validation

--- lib/features/add_event/view/add_event_screen.dart
+++ lib/features/add_event/view/add_event_screen.dart
@@ -9,7 +9,6 @@ import 'package:keym_calendar/features/calendar/bloc/calendar_bloc.dart';
 import 'package:keym_calendar/features/day_events/bloc/day_events_bloc.dart';
 import 'package:keym_calendar/features/add_event/widgets/date_time_picker.dart';
 import 'package:keym_calendar/features/add_event/widgets/image_picker.dart';
-import 'package:keym_calendar/features/add_event/widgets/save_event_button.dart';
 import 'package:keym_calendar/repositories/calendar/models/event.dart';
 
 class AddEventScreen extends StatefulWidget {
@@ -26,8 +25,9 @@ class AddEventScreen extends StatefulWidget {
 class _AddEventScreenState extends State<AddEventScreen> {
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  final _eventFormKey = GlobalKey<FormState>();
   DateTime _selectedDate = DateTime.now();
-  String? _cirrentImagePath;
+  String? _currentImagePath;
   File? _pickedImage;
 
   @override
@@ -37,7 +37,7 @@ class _AddEventScreenState extends State<AddEventScreen> {
       _titleController.text = widget.event!.title;
       _descriptionController.text = widget.event!.description;
       _selectedDate = widget.event!.dateTime;
-      _cirrentImagePath = widget.event!.photoPath;
+      _currentImagePath = widget.event!.photoPath;
     }
   }
 
@@ -52,20 +52,28 @@ class _AddEventScreenState extends State<AddEventScreen> {
   }
 
   void _saveEvent(BuildContext context) {
-    final event = Event(
-      id: widget.event?.id ?? DateTime.now().millisecondsSinceEpoch.toString(),
-      title: _titleController.text,
-      description: _descriptionController.text,
-      dateTime: _selectedDate,
-      photoPath: _pickedImage != null ? _pickedImage!.path : _cirrentImagePath,
-    );
+    if (_eventFormKey.currentState!.validate()) {
+      final event = Event(
+        id: widget.event?.id ??
+            DateTime.now().millisecondsSinceEpoch.toString(),
+        title: _titleController.text,
+        description: _descriptionController.text,
+        dateTime: _selectedDate,
+        photoPath:
+            _pickedImage != null ? _pickedImage!.path : _currentImagePath,
+      );
 
-    if (widget.event == null) {
-      BlocProvider.of<CalendarBloc>(context).add(AddEvent(event));
-    } else {
-      BlocProvider.of<CalendarBloc>(context).add(UpdateEvent(event));
-      BlocProvider.of<DayEventsBloc>(context)
-          .add(LoadEventsForDay(selectedDay: event.dateTime));
+      if (widget.event == null) {
+        BlocProvider.of<CalendarBloc>(context).add(AddEvent(event));
+      } else {
+        BlocProvider.of<CalendarBloc>(context).add(UpdateEvent(event));
+        BlocProvider.of<DayEventsBloc>(context)
+            .add(LoadEventsForDay(selectedDay: event.dateTime));
+      }
+      if (widget.isDetailScreenEdit == true) {
+        Navigator.of(context).pop();
+      }
+      Navigator.of(context).pop();
     }
   }
 
@@ -77,46 +85,75 @@ class _AddEventScreenState extends State<AddEventScreen> {
         actions: [
           Padding(
             padding: const EdgeInsets.only(right: 10),
-            child: SaveEventButton(
-              onSave: () => _saveEvent(context),
-              isDetailScreenEdit: widget.isDetailScreenEdit,
+            child: ElevatedButton(
+              onPressed: () => _saveEvent(context),
+              child: const Text('Save'),
             ),
           )
         ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            PickImage(
-              onImagePicked: _handleImagePicked,
-              currentImagePath: _cirrentImagePath,
-            ),
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Event Title'),
-            ),
-            TextField(
-              controller: _descriptionController,
-              decoration: const InputDecoration(labelText: 'Event Description'),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20),
-              child: DatePicker(
-                onDatePicked: _handleDatePicked,
-                currentDateTime: _selectedDate,
+        child: Form(
+          key: _eventFormKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              PickImage(
+                onImagePicked: _handleImagePicked,
+                currentImagePath: _currentImagePath,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                String databasesPath = await getDatabasesPath();
-                String path = join(databasesPath, 'calendar_database.db');
-                await deleteDatabase(path);
-              },
-              child: const Text('delete DB'),
-            ),
-          ],
+              TextFormField(
+                controller: _titleController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a title';
+                  }
+                  if (value.length > 70) {
+                    return 'Title should not exceed 70 characters';
+                  }
+                  return null;
+                },
+                autocorrect: false,
+                decoration: const InputDecoration(labelText: 'Event Title'),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  if (value.length > 400) {
+                    return 'Description should not exceed 400 characters';
+                  }
+                  return null;
+                },
+                autocorrect: false,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                maxLength: 400,
+                decoration: const InputDecoration(
+                  labelText: 'Event Description',
+                  counterText: null,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: DatePicker(
+                  onDatePicked: _handleDatePicked,
+                  currentDateTime: _selectedDate,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  String databasesPath = await getDatabasesPath();
+                  String path = join(databasesPath, 'calendar_database.db');
+                  await deleteDatabase(path);
+                },
+                child: const Text('delete DB'),
+              ),
+            ],
+          ),
         ),
       ),
     );
